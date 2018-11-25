1ab69e2166639c31654735de43cc061676446987
sksoumik <sadmanks@gmail.com>
2018-11-25 21:57:30 +0600

    static files added

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.home_page, name = 'home_page'),
     path('about/', views.about_page, name = 'about_page'),
     path('contact/', views.contact_page, name = 'contact_page'),
     path('login/', views.login_page, name = 'login_page'),
-    path('register/', views.register_page, name = 'register_page')
+    path('register/', views.register_page, name = 'register_page'),
+    path('admin/', admin.site.urls),
 ]
+
+
